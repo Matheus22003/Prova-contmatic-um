74733bf41062993135f44e1d0fd8fb74e2214b4f
Matheus de Oliveira Simões <45323213+Matheus22003@users.noreply.github.com>
2021-02-21 00:06:08 -0300

    Create FuncionarioTest.java

---
+++ src/test/java/br/com/contmatic/funcionario/FuncionarioTest.java
@@ -0,0 +1,48 @@
+package br.com.contmatic.funcionario;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
+
+class FuncionarioTest {
+
+	Funcionario matheus;
+
+	@BeforeEach
+	void setup() {
+		matheus = new Funcionario("Matheus", 17, "Estagiario", 2000);
+	}
+
+	@Test
+	@Timeout(1)
+	public void getName() {
+		String nomeEsperado = "Matheus";
+		assertThat("Nome incorreto", matheus.getNome(), equalTo(nomeEsperado));
+
+	}
+
+	@Test
+	@Timeout(1)
+	public void getIdade() {
+		int idadeEsperada = 17;
+		assertThat("Idade incorreta", matheus.getIdade(), equalTo(idadeEsperada));
+	}
+
+	@Test
+	@Timeout(1)
+	public void getCargo() {
+		String cargoEsperado = "Estagiario";
+		assertThat("Cargo incorreto", matheus.getCargo(), equalTo(cargoEsperado));
+	}
+
+	@Test
+	@Timeout(1)
+	public void getSalario() {
+		double salarioEsperado = 2000.00;
+		assertThat("Salario incorreto", matheus.getSalario(), equalTo(salarioEsperado));
+	}
+
+}
