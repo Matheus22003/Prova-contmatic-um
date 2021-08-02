1c7b6086047bc39ddc713a94144054da5a25abf1
Matheus de Oliveira Simões <45323213+Matheus22003@users.noreply.github.com>
2021-08-02 16:06:55 -0300

    Update Validacoes.java

--- src/main/java/br/com/contmatic/validator/Validacoes.java
+++ src/main/java/br/com/contmatic/validator/Validacoes.java
@@ -9,24 +9,24 @@ public final class Validacoes {
 
     public static void isNonNumeric(String str, String localizacaoClasse) {
         if (str.matches(".*\\d.*")) {
-            throw new IllegalArgumentException("Valor fornecido contem numeros");
+            throw new IllegalArgumentException("Valor fornecido em, " + localizacaoClasse + ", contem numeros: " + str);
         }
     }
 
     public static void isNumeric(String str, String localizacaoClasse) {
         if (!str.matches("^[0-9]*$")) {
-            throw new IllegalArgumentException("Valor fornecido contem letras");
+            throw new IllegalArgumentException("Valor fornecido em, " + localizacaoClasse + " contem letras: " + str);
         }
     }
 
     public static void isStringEmpty(String srt, String localizacaoClasse) {
         if (StringUtils.isEmpty(srt))
-            throw new IllegalArgumentException("Valor fornecido nulo: " + srt);
+            throw new IllegalArgumentException("Valor fornecido em, " + localizacaoClasse + "é nulo.");
     }
 
     public static void isStringBlank(String srt, String localizacaoClasse) {
         if (StringUtils.isAllBlank(srt))
-            throw new IllegalArgumentException("Valor fornecido nulo: " + srt);
+            throw new IllegalArgumentException("Valor fornecido em, " + localizacaoClasse + " está em branco");
     }
 
     public static void validarStringTamanhoMinimoEMaximo(String str, Integer minimo, Integer maximo, String localizacaoClasse) {
@@ -37,31 +37,31 @@ public final class Validacoes {
 
     public static void validarStringTamanhoExatoCaracteres(String str, Integer tamaho, String localizacaoClasse) {
         if (str.length() != tamaho) {
-            throw new IllegalArgumentException("A string: " + str + ", não contem: " + tamaho + "caracteres.");
+            throw new IllegalArgumentException("A string em " + localizacaoClasse + " não contem o tamanho " + tamaho + " de caracteres: " + str);
         }
     }
 
     public static void verifierObjectIsNull(Object obj, String localizacaoClasse) {
         if (obj == null) {
-            throw new NullPointerException("O Objeto está nulo");
+            throw new NullPointerException("O Objeto em " + localizacaoClasse + " está nulo");
         }
     }
 
     private static void verifierValueMinimoAndMaximo(Integer minimo, Integer maximo, String localizacaoClasse) {
         if (minimo > maximo) {
-            throw new IllegalArgumentException("Valores de minimo e maximo incorretos :" + minimo + ", " + maximo);
+            throw new IllegalArgumentException("Valores de minimo e maximo em, " + localizacaoClasse + " incorretos :" + minimo + ", " + maximo);
         }
     }
 
     public static void validarStringTamanhoMinimo(String str, Integer minimo, String localizacaoClasse) {
         if (str.length() <= minimo) {
-            throw new IllegalArgumentException("Tamanho de string está abaixo de " + minimo + " caracteres");
+            throw new IllegalArgumentException("Tamanho de string em " + localizacaoClasse + " está abaixo de " + minimo + " caracteres");
         }
     }
 
     public static void validarStringTamanhoMaximo(String str, Integer maximo, String localizacaoClasse) {
         if (str.length() >= maximo) {
-            throw new IllegalArgumentException("Tamanho de string está acima de " + maximo + " caracteres");
+            throw new IllegalArgumentException("Tamanho de string em " + localizacaoClasse + " está acima de " + maximo + " caracteres");
         }
     }
 }
